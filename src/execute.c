ad608c6f5f8c998fc11b2cba51bb14782b8eb1ab
Ranarxhag <boris.yova@gmail.com>
2019-02-23 15:05:09 +0100

    Modification parser but still not working

--- src/execute.c
+++ src/execute.c
@@ -12,43 +12,12 @@
 
 #include "minishell.h"
 
-int	parse_items(char ***instruction)
-{
-	int i;
-	char *tmp;
-
-	i = 0;
-	while ((*instruction)[i])
-	{
-		if (!(tmp = ft_strdupwc((*instruction)[i], '\t')))
-			return (0);
-		ft_strdel(&(*instruction)[i]);
-		(*instruction)[i] = tmp;
-	 	if (!(tmp = ft_strdupwc((*instruction)[i], '\v')))
-			return (0);
-		ft_strdel(&(*instruction)[i]);
-		(*instruction)[i] = tmp;
-		if (!(tmp = ft_strdupwc((*instruction)[i], '\n')))
-			return (0);
-		ft_strdel(&(*instruction)[i]);
-		(*instruction)[i] = tmp;
-		if (!(tmp = ft_strdupwc((*instruction)[i], '\r')))
-			return (0);
-		ft_strdel(&(*instruction)[i]);
-		(*instruction)[i] = tmp;
-		i++;
-	}
-	return (1);
-}
-
 int		execute_instruction(char **instruction, t_env *env)
 {
 	char	*path;
 	pid_t	pid;
 	char	**enstr;
 
-	if (!(parse_items(&instruction)))
-		return (0);
 	if (is_builtin(instruction[0]))
 		return (execute_builtin(instruction, env));
 	if (!(path = set_path(instruction, env)))
--- src/parser.c
+++ src/parser.c
@@ -15,30 +15,78 @@
 int		count_words(char *cmd)
 {
 	int i;
+	int words;
 
 	i = -1;
+	words = 0;
 	while (cmd[++i])
 	{
 		if (cmd[i] == '"')
 		{
-			i += ft_strlen(cmd) - ft_strlen(ft_strchr(&(cmd[i + 1]), '"')) - i;
+			i += ft_strlen(cmd) - ft_strclen(&(cmd[i + 1]), '"') - i;
 			continue ;
 		}
-		ft_printf("%s\n", &(cmd[i]));
+		if ((i == 0 && !ft_isspace(cmd[i])) || (ft_isspace(cmd[i]) &&
+			(!ft_isspace(cmd[i + 1]) && cmd[i + 1] != '\0')))
+			words++;
 	}
-	ft_printf("\n");
+	return (1);
+}
+
+int		create_words(char *cmd, char **tab, size_t i, int j)
+{
+	unsigned int	start;
+	size_t			length;
+	int			apo;
+
+	start = 0;
+	length = 0;
+	apo = 0;
+	while (cmd[i])
+	{
+		length = 0;
+		if ((i == 0 && !ft_isspace(cmd[i])) || (ft_isspace(cmd[i]) &&
+			(!ft_isspace(cmd[i + 1]) && cmd[i + 1] != '\0')))
+		{
+			start = (cmd[i + ((i == 0) ? 0 : 1)] == '"') ? 1 : 0;
+			if (cmd[i + ((i == 0) ? 0 : 1)] == '"')
+			{
+				length = ft_strclen(&(cmd[i + ((i == 0) ? 1 : 2)]), '"');
+				apo = 1;
+			}
+			while (!ft_isspace(cmd[i + length + ((i == 0) ? 0 : 1)]))
+				length++;
+			if (!(tab[++j] = ft_strsub(&(cmd[i + ((i == 0) ? 0 : 1)]), start, length + ((i == 0) ? 0 : 0))))
+				return (0);
+			if (apo)
+			{
+				i += ft_strclen(&(cmd[i + ((i == 0) ? 1 : 2)]), '"');
+				continue ;
+			}
+		}
+		i++;
+	}
+	tab[++j] = NULL;
 	return (1);
 }
 
 char	**parse_cmd(char *cmd)
 {
 	char	**tab;
+	char	*tmp;
 
 	if (cmd == NULL)
 		return (NULL);
+	if (!(tmp = ft_strdupwstr(cmd, "\"\"")))
+		return (NULL);
+	ft_strdel(&cmd);
+	cmd = tmp;
 	if(!(tab = (char**)malloc(sizeof(char*) * (count_words(cmd) + 1))))
 		return (NULL);
-	tab[count_words(cmd)] = NULL;
+	if (!(create_words(cmd, tab, 0, -1)))
+	{
+
+	}
 	return (tab);
 }
 
@@ -47,14 +95,13 @@ int		main()
 	char **tab;
 	int i;
 
-	tab = parse_cmd("echo \"Coucou\" Je lol \"Salut\"");
+	//tab = parse_cmd(ft_strdup("      echo coucou les  gens \"coucou      les       gens\"\"coucou\"abcdef       "));
+	tab = parse_cmd(ft_strdup("echo \"Bonjour\""));
 	i = 0;
-	/*
 	while (tab[i])
 	{
 		ft_printf("%s\n", tab[i]);
 		i++;
 	}
-	*/
 	return (0);
 }
